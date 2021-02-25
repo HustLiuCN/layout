9eb79962dad06b95359c19343685544c79787f3e
liupeidong <liupeidong@sensorsdata.cn>
2021-02-25 19:54:17 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path')
+
+const webpackConfig = {
+	mode: process.env.NODE_ENV || 'production',
+	// watch: true,
+	entry: {
+		layout: './src/demo/layout',
+	},
+	output: {
+		filename: '[name].build.js',
+		path: path.resolve(__dirname, './demos/dist'),
+	},
+	// TODO
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				loader: 'ts-loader',
+			}, {
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: [
+					'babel-loader',
+				],
+			}, {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+		]
+	},
+	resolve: {
+		extensions: ['.js', '.json', '.ts'],
+		alias: {
+			'@data': path.resolve(__dirname, './mock-data'),
+			'@style': path.resolve(__dirname, './style'),
+			'@lib': path.resolve(__dirname, './lib'),
+			'@src': path.resolve(__dirname, './src'),
+		},
+	},
+	devtool: 'sourcemap',
+}
+
+module.exports = webpackConfig
