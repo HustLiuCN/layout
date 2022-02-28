3cabc2a4a35242c8b7d52b13cef7dd17c5565555
liupeidong <liupeidong@sensorsdata.cn>
2022-02-28 15:58:44 +0800

    .

--- src/wave.ts
+++ src/wave.ts
@@ -4,8 +4,10 @@ import colors from './color';
 import { _distance } from './utils';
 
 const { get } = axios;
-const R = 100;
+const R = 10;
 const D = 1;
+
+const RANGE_Y = 100;
 
 const PI = Math.PI;
 
@@ -26,29 +28,23 @@ class Wave {
 
   init = () => {
     // this.fetchData().then(() => this.render());
-    const { cvs, step } = this;
+    const { cvs, step, } = this;
     const { rect } = cvs;
-    const xn = 1;
-    const map = [];
-    for (let x = 0; x < xn; x ++) {
-      const yn = 10;
-      const list = [];
-      for (let y = 0; y < yn; y ++) {
-        const order = (y + step + x) % 10;
-        const sin = Math.sin(2 * PI * order / 10);
-        const node: ICircle = {
-          id: `${x}-${y}`,
-          x: rect.width / 2 + (x - xn / 2) * (rect.width / 10 - y * 2),
-          y: rect.height - (y + 1) * 20 - sin * 10,
-          r: Math.abs(8 * sin) + 1,
-          fillColor: colors[0],
-        };
-        list.push(node);
-      }
-      // this.nodes = list;
-      map.push(list);
+
+    const nodes = [];
+    const n = 20;
+    for (let i = 0; i < n; i ++) {
+      const scale = (i + step) % 10;
+      const sin = Math.sin(2 * PI * scale / 10)
+      const node: ICircle = {
+        id: `${i}`,
+        x: rect.width * (i + 1) / (n + 1),
+        y: rect.height / 2 - i * 4 + sin * (RANGE_Y - (i / n) * 20),
+        r: R - i / n * 4,
+      };
+      nodes.push(node);
     }
-    this.map = map;
+    this.nodes = nodes;
   }
 
   ani = () => {
@@ -66,18 +62,18 @@ class Wave {
 
   /** 波浪 */
   render = () => {
-    const { edges, temps, cvs, step, map } = this;
+    const { edges, temps, cvs, step, map, nodes } = this;
     cvs.clear();
     // if (step < PI * 2) {
     //   this.step += (PI * 2 / 60);
     // } else {
     //   this.step = 0;
     // }
-    this.step += 0.1;
+    this.step += 0.02;
     // 绘制节点
-    map.forEach(nodes => {
+    // map.forEach(nodes => {
       nodes.forEach(node => cvs.paintCircle(node));
-    });
+    // });
 
     this.init();
 
